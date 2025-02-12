c55c142364d10430c7ea1696e77e00185e999a3d
jeffreyai7 <jirukeh@gmail.com>
2025-02-12 07:53:16 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='underline text-2xl text-center text-blue-500 bg-gray-200'>
+      hi it is me
+    </div>
+  )
+}
+
+export default App
