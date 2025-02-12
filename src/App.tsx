6c66d11fc1acbaf0a150e4ffa7c38a234621908b
jeffreyai7 <jirukeh@gmail.com>
2025-02-12 11:57:05 +0100

    finish Attendee details structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
+import AttendeeDetails from './components/AttendeeDetails'
 import NavBar from './components/NavBar'
+import TicketSelection from './components/TicketSelection'
 function App() {
 
   return (
     <main>
       <NavBar/>
+      <TicketSelection/>
+      <AttendeeDetails/>
     </main>
   )
 }
--- src/components/AttendeeDetails.tsx
+++ src/components/AttendeeDetails.tsx
@@ -1,7 +1,44 @@
 
 const AttendeeDetails = () => {
   return (
-    <div>AttendeeDetails</div>
+    <section>
+     <div>
+        <div>
+          <h1>Attendee Details</h1>
+          <span>step 2/3</span>
+          </div>
+          <div className="progressbar"></div>
+     </div>
+     <div>
+        <div>
+            <span>Upload Profile Photo</span>
+            <div>Image upload</div>
+        </div>
+        <div className="division"></div>
+        <div>
+            <div>
+                <label htmlFor="name">Enter your name</label>
+                <input type="text" name="fullName"  id="name"/>
+            </div>
+            <div>
+                <label htmlFor="email">Enter your email</label>
+                <div><input type="email" name="email" id="email"/></div>
+            </div>
+            <div>
+                <label htmlFor="textarea">Special request?</label>
+                <textarea name="textarea" id="textarea" placeholder="Textarea"></textarea>
+            </div>
+            <div>
+                <button>Back</button>
+                <button>Get My Free Ticket</button>
+            </div>
+        </div>
+     </div>
+
+
+
+
+    </section>
   )
 }
 
